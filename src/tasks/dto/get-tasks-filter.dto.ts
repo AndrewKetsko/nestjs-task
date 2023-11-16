9bc6bc013f91a7534f038b77a78e0564bfd70b8a
Andrew Ketsko <monicman82@gmail.com>
2023-11-16 12:42:44 +0200

    some pipes added

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
 import { TaskStatus } from '../task.model';
+import { IsString, IsNotEmpty, IsOptional, IsIn } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
